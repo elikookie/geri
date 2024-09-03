2a01a514f3dc522a76dd14b464ba9cba1e4b9025
Elitsa Koleva <53860330+elikookie@users.noreply.github.com>
2024-09-03 22:07:44 +0100

    forgot to build oops

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Geri Portfolio",
   description: "Geri Portfolio",
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
